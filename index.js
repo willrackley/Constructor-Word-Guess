71c58b9c288facd36851e42728692e5bfc297e27
williamrackley <willrackley65@gmail.com>
2019-02-07 11:44:55 -0500

    added functionality where user can only type a letter

--- index.js
+++ index.js
@@ -1,20 +1,19 @@
 var Words = require("./Word");
 var inquirer = require("inquirer");
 
-var gameWords = ["lexus", "audi" ,"bentley", "toyota"];
-var randWordSelect = Math.floor(Math.random() * gameWords.length);
+var gameWords = ["lexus", "audi" ,"bentley", "toyota", "jaguar"];
 var indexCtr = 0;
+var winCtr = 0;
+var alphabet = "abcdefghijklmnopqrstuvwxyz";
 var chosenWordToSend = gameWords[indexCtr];
-var correctLetters = [];
 var wrongLetters = [];
-
 var wordCheckArray = [];
 var guessesRemaining = 7;
 var word = new Words(chosenWordToSend);
 
 
-
 var continuePlay = function(){
+   
 
     if(guessesRemaining > 0){
    
@@ -26,6 +25,7 @@ var continuePlay = function(){
         }
     ]).then(function(answers) {
         var checkedchar = answers.GuessLetter.toLowerCase();
+        
         wordCheckArray = [];
         for(i=0; i < word.newLetters.length; i++){
             word.wordGuess(checkedchar);
@@ -34,17 +34,24 @@ var continuePlay = function(){
         
        if(chosenWordToSend.includes(checkedchar)){
         console.log("\nCORRECT!");
-        correctLetters.push(checkedchar);
+
+       } else if (!chosenWordToSend.includes(checkedchar) && !alphabet.includes(checkedchar)){
+        console.log("\nPLEASE TYEP A LETTER FROM A-Z");
        } else {
         console.log("\nINCORRECT!");
         guessesRemaining--;
         wrongLetters.push(checkedchar);
        }
 
-       
        if(wordCheckArray.indexOf(false) === -1){
-            guessesRemaining = 0;
+           guessesRemaining = 0;
+           winCtr++;
        }
+
+       if(winCtr === 5){
+           guessesRemaining = 0; 
+       }
+
        console.log('\n========================');
        word.wordDisplay();
        console.log("\nyou have " + guessesRemaining + " guesses remaining");
@@ -54,93 +61,116 @@ var continuePlay = function(){
     
         continuePlay();
         function wordCheck(key){
-            wordCheckArray.push(key.guessed);
-            
+            wordCheckArray.push(key.guessed); 
         }
     });
-} else if(guessesRemaining === 0 && wordCheckArray.indexOf(false) !== -1){
-    inquirer.prompt([
-        {
-            name: "gameOver",
-            type: "list",
-            message: "GAME OVER!",
-            choices: ["PLAY AGAIN", "END GAME"]
-            
-        }
-    ]).then(function(answers) {
-        if(answers.gameOver === "PLAY AGAIN"){
-            indexCtr = 0;
-            chosenWordToSend = gameWords[indexCtr];
-            word = new Words(chosenWordToSend);
-            guessesRemaining = 7;
-            wrongLetters = [];
-            correctLetters = [];
 
-            userGuess();
-        }
+    } else if(guessesRemaining === 0 && winCtr === 5){
+        
+        inquirer.prompt([
+            {
+                name: "completedGame",
+                type: "list",
+                message: "Great Job! You've Completed the game!",
+                choices: ["PLAY AGAIN", "END GAME"]
+                
+            }
+        ]).then(function(answers) {
+            if(answers.completedGame === "PLAY AGAIN"){
+                indexCtr = 0;
+                winCtr = 0;
+                chosenWordToSend = gameWords[indexCtr];
+                word = new Words(chosenWordToSend);
+                guessesRemaining = 7;
+                wrongLetters = [];
+                correctLetters = [];
 
-    });
-   
-} else if (guessesRemaining === 0 && wordCheckArray.indexOf(false) === -1){
+                userGuess();
+            }
+        }); 
+
+    } else if(guessesRemaining === 0 && wordCheckArray.indexOf(false) !== -1){
+        inquirer.prompt([
+            {
+                name: "gameOver",
+                type: "list",
+                message: "GAME OVER!",
+                choices: ["PLAY AGAIN", "END GAME"]
+                
+            }
+        ]).then(function(answers) {
+            if(answers.gameOver === "PLAY AGAIN"){
+                indexCtr = 0;
+                winCtr = 0;
+                chosenWordToSend = gameWords[indexCtr];
+                word = new Words(chosenWordToSend);
+                guessesRemaining = 7;
+                wrongLetters = [];
+                correctLetters = [];
+
+                userGuess();
+            }
+
+        });
     
-    inquirer.prompt([
-        {
-            name: "nextWord",
-            type: "list",
-            message: "Great Job! Would you like to play another word?",
-            choices: ["YES", "NO"]
+    } else if (guessesRemaining === 0 && wordCheckArray.indexOf(false) === -1){
+        
+        inquirer.prompt([
+            {
+                name: "nextWord",
+                type: "list",
+                message: "Great Job! Would you like to play another word?",
+                choices: ["YES", "NO"]
+                
+            }
+        ]).then(function(answers) {
+            if(answers.nextWord === "YES"){
+                indexCtr++;
+                chosenWordToSend = gameWords[indexCtr];
+                word = new Words(chosenWordToSend);
+                guessesRemaining = 7;
+                wrongLetters = [];
+                correctLetters = [];
+
+                console.log('\n========================');
+                word.wordDisplay();
+                console.log("\nyou have " + guessesRemaining + " guesses remaining")
+                console.log('\n\n========================');
+                console.log("\n");
+
+                continuePlay();
+                
+            }
             
-        }
-    ]).then(function(answers) {
-        if(answers.nextWord === "YES"){
-            indexCtr++;
-            chosenWordToSend = gameWords[indexCtr];
-            word = new Words(chosenWordToSend);
-
-            guessesRemaining = 7;
-            wrongLetters = [];
-            correctLetters = [];
-
-            console.log('\n========================');
-            word.wordDisplay();
-            console.log("\nyou have " + guessesRemaining + " guesses remaining")
-            console.log('\n\n========================');
-            console.log("\n");
-
-            continuePlay();
-            
-        }
-
-    });
+        });
+    } 
 }
-}
-  
-
+    
 
 var userGuess = function(){   
     
-inquirer.prompt([
-    {
-      name: "initialQuestion",
-      message: "Welome to 'Guess That Car'! \n Guess the make of a car one letter at a time. \n Wanna Play?",
-      type: "list",
-      choices: ["YES", "NO"]
-    }, 
-    
-]).then(function(answers) {
-    if(answers.initialQuestion === "YES"){
-        console.log('\n========================');
-       word.wordDisplay();
-       console.log("\nyou have " + guessesRemaining + " guesses remaining");
-       console.log("\nletters you've guessed incorrectly: " + wrongLetters.join(' '));
-       console.log('\n\n========================');
-       console.log("\n");
-        continuePlay();
-    } else {
-        return;
-    }
-    });
-    
+    inquirer.prompt([
+        {
+        name: "initialQuestion",
+        message: "Welome to 'Guess That Car'! \n Guess the make of a car one letter at a time. \n Wanna Play?",
+        type: "list",
+        choices: ["YES", "NO"]
+        }, 
+        
+    ]).then(function(answers) {
+        if(answers.initialQuestion === "YES"){
+            console.log('\n========================');
+        word.wordDisplay();
+        console.log("\nyou have " + guessesRemaining + " guesses remaining");
+        console.log("\nletters you've guessed incorrectly: " + wrongLetters.join(' '));
+        console.log('\n\n========================');
+        console.log("\n");
+            continuePlay();
+        } else {
+            return;
+        }
+    });  
 }
+
 
 userGuess();
